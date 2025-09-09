28fbc680fb88e8b160f9411e6038a52d2483cc12
jack37young <jack37young@gmail.com>
2025-09-09 14:47:49 -0500

    Update app_johnny.py

--- app_johnny.py
+++ app_johnny.py
@@ -8,16 +8,34 @@ if "page" not in st.session_state:
 def go_to_page(page_name):
     st.session_state.page = page_name
 
+if st.button("Blue Raspberry"):
+        flavor = "Blue Raspberry"
+    elif st.button("Tiger Blood"):
+        flavor = "Tiger Bloode"
+    elif st.button("Banana"):
+        flavor = "Banana"
+    elif st.button("Wedding Cake"):
+        flavor = "Wedding Cake"
+    elif st.button("Strawberry Lemonade"):
+        flavor = "Strawberry Lemonade"
+    elif st.button("Sour Apple"):
+        flavor = "Sour Apple"
+    elif st.button("Cotton Candy"):
+        flavor = "Cotton Candy"
+    elif st.button("Arctic Blast"):
+        flavor = "Arctic Blast"
+
+
 # Page 1: Home page with "Blueberry" button
 if st.session_state.page == "home":
     st.title("Welcome!")
-    if st.button("Blueberry"):
-        go_to_page("blueberry")
+    if st.button(flavor):
+        go_to_page(flavor)
     
 
 # Page 2: Number input page
-elif st.session_state.page == "blueberry":
-    st.title("Blueberry")
+elif st.session_state.page == flavor:
+    st.title(flavor)
     
     # Number input
     number = st.number_input("Enter a number:", value=0)
